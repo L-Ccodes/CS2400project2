11b24d6ba353df9d09ce6d5c5f707af3f88abfa4
Luis Navarrete <lanavarrete@cpp.edu>
2021-10-10 22:00:56 -0700

    luis second commit

--- CS2400Project2/src/ArrayStackTest.java
+++ CS2400Project2/src/ArrayStackTest.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class ArrayStackTest {
 	public static void main(String[] args) {
@@ -7,7 +8,50 @@ public class ArrayStackTest {
 		System.out.println("Enter postfix expression: ");
 		postfix = scan.nextLine();
 
-		System.out.println(ResizeableArrayStack.evaluatePostfix(postfix));
+		System.out.println(evaluatePostfix(postfix));
 		scan.close();
+	}
+	
+	public static double evaluatePostfix(String postfix) {
+		Stack<Integer> valueStack = new Stack<>();
+		int result = 0;
+		char nextCharacter;
+		int i = 0;
+		while(i < postfix.length()) {
+			nextCharacter = postfix.charAt(i++); 
+			if(Character.isDigit(nextCharacter)) {
+				valueStack.push(Character.getNumericValue(nextCharacter));
+			}
+			else if(nextCharacter != ' '){
+				int operandTwo = valueStack.pop();
+				int operandOne = valueStack.pop();
+
+				switch(nextCharacter) {
+				case '+':
+					result = operandOne + operandTwo;      
+					break;
+				case '-':
+					result = operandOne - operandTwo;      
+					break;
+				case '*':
+					result = operandOne * operandTwo;     
+					break;
+				case '/':
+					result = operandOne / operandTwo;      
+					break;
+				case '^':
+					result = (int) Math.pow(operandOne, operandTwo);      
+					break;
+				default:
+					break;
+
+				}
+				valueStack.push(result); 
+			}
+			else {
+				continue;
+			}
+		}
+		return valueStack.peek();       
 	}
 }
--- CS2400Project2/src/ResizeableArrayStack.java
+++ CS2400Project2/src/ResizeableArrayStack.java
@@ -1,47 +1,82 @@
-import java.util.Stack;
+import java.util.Arrays;
+import java.util.EmptyStackException;
 
-public class ResizeableArrayStack {
-	public static double evaluatePostfix(String postfix) {
-		Stack<Integer> valueStack = new Stack<>();
-		int result = 0;
-		char nextCharacter;
-		int i = 0;
-		while(i < postfix.length()) {
-			nextCharacter = postfix.charAt(i++); 
-			if(Character.isDigit(nextCharacter)) {
-				valueStack.push(Character.getNumericValue(nextCharacter));
-			}
-			else if(nextCharacter != ' '){
-				int operandTwo = valueStack.pop();
-				int operandOne = valueStack.pop();
+public final class ResizeableArrayStack<T> implements StackInterface<T>{
 
-				switch(nextCharacter) {
-				case '+':
-					result = operandOne + operandTwo;      
-					break;
-				case '-':
-					result = operandOne - operandTwo;      
-					break;
-				case '*':
-					result = operandOne * operandTwo;     
-					break;
-				case '/':
-					result = operandOne / operandTwo;      
-					break;
-				case '^':
-					result = (int) Math.pow(operandOne, operandTwo);      
-					break;
-				default:
-					break;
+	private T[] stack;
+	private int topIndex;
+	private boolean integrityOK = false;
+	private static final int DEFAULT_CAPACITY = 50;
+	private static final int MAX_CAPACITY = 10000;
 
-				}
-				valueStack.push(result); 
-			}
-			else {
-				continue;
-			}
+
+	public ResizeableArrayStack(){
+		this(DEFAULT_CAPACITY);
+	}
+
+	public ResizeableArrayStack(int initialCapacity)
+	{
+		integrityOK = false;
+		checkCapacity(initialCapacity);
+		@SuppressWarnings("unchecked")
+		T[] tempStack = (T[])new Object[initialCapacity];
+		stack = tempStack;
+		topIndex = -1;
+		integrityOK = true;
+
+	}
+	private void checkCapacity(int capacity) {
+		if (capacity > MAX_CAPACITY)
+			throw new IllegalStateException("Attempt to create a bag whose " +"capacity exeeds allowed " +"maximum of " + MAX_CAPACITY);
+	}
+
+	public void push(T newEntry){
+		ensureCapacity();
+		stack[topIndex + 1] = newEntry;
+		topIndex++;
+
+	}
+	private void ensureCapacity(){
+		if (topIndex == stack.length - 1){
+			int newLength = 2 * stack.length;
+			checkCapacity(newLength);
+			stack = Arrays.copyOf(stack, newLength);
 		}
-		return valueStack.peek();       
+	}
+	public T peek(){
+		if (isEmpty())
+			throw new EmptyStackException();
+		else
+			return stack[topIndex];
+	}
+	public T pop(){
+
+		if (isEmpty())
+			throw new EmptyStackException();
+		else{
+			T top = stack[topIndex];
+			stack[topIndex] = null;
+			topIndex--;
+			return top;
+		} 
+	}
+	public boolean isEmpty(){
+		return topIndex < 0;
+	}
+
+	private void checkIntegrity() {
+		//Checks to see if integrityOk is false.
+		if(!integrityOK) {
+			throw new SecurityException("Object is corrupt");
+		}//end if statement
+	}// end checkIntegrity
+
+	public void clear() {
+		checkIntegrity();
+		while (topIndex > -1) {
+			stack[topIndex] = null;
+			topIndex--;
+		}
 	}
 
 }
