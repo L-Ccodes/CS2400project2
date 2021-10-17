45032c50fe2fb8296b83c29f8367c99e80a0f48e
Luis Navarrete <lanavarrete@cpp.edu>
2021-10-17 15:38:33 -0700

    add evaluate

--- CS2400Project2/src/evaluatePostfixTest.java
+++ CS2400Project2/src/evaluatePostfixTest.java
@@ -1,12 +1,21 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-public class JUnitTest {
+/** Tests the methods inside ArrayStackTest
+ * 
+ * @author Luis Navarrete
+ *
+ */
+public class evaluatePostfixTest {
 
 	@Test
+	/** Tests the evaluatePostfix method.
+	 * 
+	 */
 	public void test() {
-		fail("Not yet implemented");
+		
+		double output = ArrayStackTest.evaluatePostfix("23*42-/56*+");
+		assertEquals(33.0, output, 0.0);
 	}
 
 }
