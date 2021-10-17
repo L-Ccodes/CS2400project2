46525f05aee628979a09dfe44975a685a178c50c
Luis Navarrete <lanavarrete@cpp.edu>
2021-10-16 20:05:14 -0700

    add comments

--- CS2400Project2/src/ArrayStackTest.java
+++ CS2400Project2/src/ArrayStackTest.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
 import java.util.Stack;
 
+
 public class ArrayStackTest {
+	
+	/** This is the main method
+	 * @param args is the argument for the main method
+	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String postfix;
@@ -12,20 +17,29 @@ public class ArrayStackTest {
 		scan.close();
 	}
 	
+	/** This converts the postfix string to the evaluation.
+	 * @param postfix is the initial string of the expression.
+	 * @return result, returns the value of the postfix expression.
+	 */
 	public static double evaluatePostfix(String postfix) {
 		Stack<Integer> valueStack = new Stack<>();
 		int result = 0;
 		char nextCharacter;
 		int i = 0;
+		//while loop to go through expression
 		while(i < postfix.length()) {
 			nextCharacter = postfix.charAt(i++); 
+			
+			//checks if next character is a digit
 			if(Character.isDigit(nextCharacter)) {
 				valueStack.push(Character.getNumericValue(nextCharacter));
 			}
+			//checks if next character is a space
 			else if(nextCharacter != ' '){
 				int operandTwo = valueStack.pop();
 				int operandOne = valueStack.pop();
-
+				
+				//switch statement that checks what the operation is
 				switch(nextCharacter) {
 				case '+':
 					result = operandOne + operandTwo;      
@@ -45,13 +59,13 @@ public class ArrayStackTest {
 				default:
 					break;
 
-				}
+				}//end switch 
 				valueStack.push(result); 
-			}
+			}//end if
 			else {
 				continue;
 			}
-		}
+		}//end while loop
 		return valueStack.peek();       
 	}
 }
--- CS2400Project2/src/ResizeableArrayStack.java
+++ CS2400Project2/src/ResizeableArrayStack.java
@@ -1,18 +1,25 @@
 import java.util.Arrays;
 import java.util.EmptyStackException;
 
+/** Class created to create a stack
+*
+* @param <T> is a generic data type. The user can choose the data type that
+* will be used.
+*/
 public final class ResizeableArrayStack<T> implements StackInterface<T>{
-
+	//private fields
 	private T[] stack;
 	private int topIndex;
 	private boolean integrityOK = false;
 	private static final int DEFAULT_CAPACITY = 50;
 	private static final int MAX_CAPACITY = 10000;
 
-
+	/** Constructor for a creates stack.
+	 * 
+	 */
 	public ResizeableArrayStack(){
 		this(DEFAULT_CAPACITY);
-	}
+	}// end default constructor
 
 	public ResizeableArrayStack(int initialCapacity)
 	{
@@ -25,11 +32,17 @@ public final class ResizeableArrayStack<T> implements StackInterface<T>{
 		integrityOK = true;
 
 	}
+	/* Checks the capacity of the stack
+	 * 
+	 */
 	private void checkCapacity(int capacity) {
 		if (capacity > MAX_CAPACITY)
 			throw new IllegalStateException("Attempt to create a bag whose " +"capacity exeeds allowed " +"maximum of " + MAX_CAPACITY);
 	}
-
+	
+	/** Pushes a new entry into the stack
+     *  @param newEntry is the entry that will be pushed into the stack.
+     */
 	public void push(T newEntry){
 		ensureCapacity();
 		stack[topIndex + 1] = newEntry;
@@ -43,12 +56,20 @@ public final class ResizeableArrayStack<T> implements StackInterface<T>{
 			stack = Arrays.copyOf(stack, newLength);
 		}
 	}
+	
+	/** A method designed to see what is at the top of the stack.
+	 * @return what is at the top of the stack.
+	 */
 	public T peek(){
 		if (isEmpty())
 			throw new EmptyStackException();
 		else
 			return stack[topIndex];
 	}
+	
+	/** Pops (or takes out) the top entry of the stack.
+	 * @return top returns what entry is popped out.
+	 */
 	public T pop(){
 
 		if (isEmpty())
@@ -60,10 +81,17 @@ public final class ResizeableArrayStack<T> implements StackInterface<T>{
 			return top;
 		} 
 	}
+	
+	/** Checks to see if the stack is empty.
+	 * @return null.
+	 */
 	public boolean isEmpty(){
 		return topIndex < 0;
 	}
 
+	/** Checks the security of the stack that has been passed by the user.
+	    * 
+	    */
 	private void checkIntegrity() {
 		//Checks to see if integrityOk is false.
 		if(!integrityOK) {
@@ -71,6 +99,9 @@ public final class ResizeableArrayStack<T> implements StackInterface<T>{
 		}//end if statement
 	}// end checkIntegrity
 
+	/** Clears out the entire stack.
+	 * @return null.
+	 */
 	public void clear() {
 		checkIntegrity();
 		while (topIndex > -1) {
